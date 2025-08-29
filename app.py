63b255cbc80bfff8f9c5b620af663e1c9520f436
tushar.oclock <talkativetushar@gmail.com>
2025-08-30 03:07:40 +0530

    commit

--- app.py
+++ app.py
@@ -346,7 +346,7 @@ def display_home(chapter_names):
 
 def main():
     # Page configuration
-    st.set_page_config(page_title="Supercalifragilisticexpialidocious", page_icon="💀")
+    st.set_page_config(page_title="Supercalifragilisticexpialidocious", page_icon="💀", layout="wide")
 
 
 
